99dd2f2787d5166f839475ca2adbb8ff41b963a6
Dior <yuldashev.dior@mail.ru>
2023-10-09 13:42:57 +0500

    Решения функций

---
+++ js/functions.js
@@ -0,0 +1,40 @@
+function checkStringLength (str,len) {
+  if (str.length <= len){
+    return true;
+  }
+  return false;
+}
+checkStringLength('проверяемая строка', 10);
+
+function checkPalindrome (str) {
+  let string = str.replaceAll(' ','');
+  string = string.toLowerCase();
+
+  let reverseStr = '';
+  for (let i = string.length - 1; i >= 0; i = i - 1) {
+    reverseStr += string.at(i);
+  }
+  if (string === reverseStr) {
+    return true;
+  }
+  return false;
+}
+checkPalindrome('Лёша на полке клопа нашёл ');
+
+function extractNumbers (str) {
+  let newString = '';
+  for (let i = 0; i <= str.length - 1; i++) {
+    if (Number(str[i]) || Number(str[i]=== '0')) {
+      newString +=  str[i];
+    }
+  }
+  if (newString === '') {
+    return NaN;
+  }
+  return Number(newString);
+}
+extractNumbers('2023 год');
+extractNumbers('ECMAScript 2022');
+extractNumbers('1 кефир, 0.5 батона');
+extractNumbers('агент 007');
+extractNumbers('а я томат');
